7bf8aa225881361fc35e62f5509fc511cd0e3964
Exequiel Leandro Morelli <exemorelli@gmail.com>
2022-09-12 01:22:07 -0300

    commit final 1er entrega del proyecto final

--- js/main.js
+++ js/main.js
@@ -1,1 +1,87 @@
-alert("Prueba")
+class Personal {
+    constructor(nombre, sector) {
+        this.nombre = nombre.toUpperCase();
+        this.sector = sector.toUpperCase();
+        this.estado = "ACTIVO";
+    }
+}
+
+//Declaro un array para guardar al personal registrado
+const personal = [];
+const sectores = [
+    { id: 1, sector: "Preparación" },
+    { id: 2, sector: "Fabricación" },
+    { id: 3, sector: "Pintura" },
+    { id: 4, sector: "Puesta" }
+];
+
+// Función para realizar la carga de un nuevo empleado
+function entradaPersonal() {
+    let texto = "";
+    let nombre = prompt("Ingrese el nombre");
+    for (const sector of sectores) {
+        texto = `${texto}${sector.id}. ${sector.sector}\n`;
+    }
+    let idSector = prompt(`Ingrese el sector:\n${texto}`);
+    idSector = Number(idSector);
+    if (validarSector(idSector)) {
+        personal.push(new Personal(nombre, sectores[idSector - 1].sector));
+    } else {
+        alert("Valor inválido")
+    }
+};
+
+// Función para validar que el ID del sector existe
+function validarSector(sector) {
+    if (sector <= sectores.length && sector > 0) {
+        return true;
+    }
+}
+
+// Pregunto si quiere seguir cargando empleados a la plataforma
+function continuar() {
+    let comprobacion = prompt("¿Desea repetir el proceso? (S/N)")
+    switch (comprobacion) {
+        case "S":
+            return true;
+        case "s":
+            return true;
+        default:
+            return false;
+    }
+}
+
+// Antes de terminar, consulto si quiere realizar una búsqueda
+function consultarBusqueda() {
+    let comprobacion = prompt("¿Desea realizar una búsqueda dentro del personal? (S/N)")
+    switch (comprobacion) {
+        case "S":
+            return true;
+        case "s":
+            return true;
+        default:
+            return false;
+    }
+}
+
+// Función para realizar una búsqueda dentro del personal
+function buscarPersonal() {
+    let consultarNombre = prompt("Ingrese el nombre que desea buscar");
+    consultarNombre = consultarNombre.toUpperCase();
+    const buscar = personal.find((persona) => persona.nombre === consultarNombre);
+    // console.log(buscar);
+    if (typeof (buscar) == "object") {
+        alert(`Nombre: ${buscar.nombre}\nSector: ${buscar.sector}\nEstado: ${buscar.estado}`)
+    } else {
+        alert("No se encontraron coincidencias.")
+    }
+}
+
+
+
+// CÓDIGO PRINCIPAL
+do {
+    entradaPersonal();
+} while (continuar());
+
+buscarPersonal();
